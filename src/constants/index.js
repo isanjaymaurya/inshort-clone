f28df21cbd28175aa78bd38471447a6234628f32
isanjaymaurya <its.sanjaymaurya@gmail.com>
2023-01-14 12:51:12 +0530

    added cat

---
+++ src/constants/index.js
@@ -0,0 +1,32 @@
+export const NewsCat = [
+    {
+        title: 'Hardware',
+        value: 'hardware',
+        desc: 'news about the latest smartphones, laptops, tablets, and other consumer electronics.'
+    },
+    {
+        title: 'Software and apps',
+        value: 'software',
+        desc: 'news about new and updated software, operating systems, and mobile apps.'
+    },
+    {
+        title: 'Gaming',
+        value: 'gaming',
+        desc: 'news about the latest video games, gaming consoles, and gaming-related technology.'
+    },
+    {
+        title: 'Crypto',
+        value: 'crypto',
+        desc: 'This includes news about new and updated software, operating systems, and mobile apps.'
+    },
+    {
+        title: 'Artificial Intelligence and Machine Learning',
+        value: 'ai',
+        desc: 'This includes news about new and ai and machine learning.'
+    },
+    {
+        title: 'Cybersecurity',
+        value: 'cybersecurity',
+        desc: 'This includes news about new and updated software, operating systems, and mobile apps.'
+    },
+]
